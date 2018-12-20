83ce1ea706b5c6d88f6950554b9c9f8c26add7f9
AYH4721陈实 <chenshi3@centaline.com.cn>
2018-12-20 11:53:29 +0800

    添加 分治法 demo 。 Bsearch & 汉诺塔 demo

---
+++ algorithm/BSearch/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BSearch
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int index = BSearch(arr, 0, arr.Length - 1, 6);
+            Console.WriteLine(index);
+            Console.ReadKey();
+        }
+
+        public static int BSearch(int[] data, int left, int right, int key)
+        {
+            int middle = (left + right) / 2;
+
+            if (data[middle] == key)
+            {
+                return key;
+            }
+            else if (data[middle] > key)
+            {
+                return BSearch(data, left, middle - 1, key);
+            }
+            else
+            {
+                return BSearch(data, middle + 1, right, key);
+            }
+        }
+    }
+}
---
+++ algorithm/HannoTower/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace HannoTower
+{
+    class Program
+    {
+        public static int count = 1;
+        static void Main(string[] args)
+        {
+            Moved(4, "第一根柱子", "第二根柱子", "第三根柱子");
+        }
+
+        /// <summary>
+        /// 移动
+        /// </summary>
+        /// <param name="i">初始圆盘数量</param>
+        /// <param name="a">圆盘初始所在位置</param>
+        /// <param name="b">圆盘要移动的塔座</param>
+        /// <param name="c">辅助圆盘要移动的塔座</param>
+        public static void Moved(int i, string a, string b, string c)
+        {
+            if (i == 1)
+            {
+                Display(i, a, b);
+            }
+            else
+            {
+                // 将 i-1 根圆盘由A移动到C
+                Moved(i - 1, a, c, b);
+                // 将圆盘 i 由 A 移动 到 B
+                Display(i, a, b);
+                // 将 i-1 根圆盘由C移动到A
+                Moved(i - 1, c, b, a);
+            }
+        }
+
+        public static void Display(int i, string a, string b)
+        {
+            Console.WriteLine($"第{count}步:移动{i}号圆盘，从{a}到{b}");
+            count++;
+        }
+    }
+}
